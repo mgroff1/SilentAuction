c2a00158b6591582c907e03e14410b06590d7888
mgroff1 <mick.groff@gmail.com>
2019-11-13 12:21:47 -0700

    Created-less-folder-better-skelatal-start-point

---
+++ js/product.js
@@ -0,0 +1,16 @@
+
+function open(){
+    $('.slide').hide("fade",200);
+$('#cover').show( "slide", { direction: "up"  }, 500 );
+contain.show( "slide", { direction: "up"  }, 500 );
+    arr.forEach(function (el) {
+        el.hide( "slide", { direction: "down"  }, 500 );
+    });
+
+    let clicked = $(this);
+    for(let i =0; i < 4; i++){
+
+        if(clicked.data("match") === arr[i].data("match")){
+            arr[i].show( "slide", { direction: "up"  }, 500 );
+        }
+   }}
